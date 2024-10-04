822362b57b3561969bc44feecd4d36946629407e
insomnic123 <qaziayn@gmail.com>
2024-10-04 11:19:58 -0400

    fixed bugs

--- src/Main.java
+++ src/Main.java
@@ -61,7 +61,21 @@ public class Main {
 
     static Scanner scanner = new Scanner(System.in);
 
-    static int score = 0;
+    static List<Integer> times = new ArrayList<>();
+
+    static int score;
+
+    public static void calculateScore(int rounds) {
+        int sum = 0;
+        for (Integer time : times) {
+            sum += time;
+        }
+
+        double averageTime = sum/ (double) rounds;
+
+        score = (int) ((100/(averageTime + 100))*1000);
+
+    }
 
     // I found this really cool thing here https://medium.com/javarevisited/how-to-display-progressbar-on-the-standard-console-using-java-18f01d52b30e
     // and wanted to include it :(((
@@ -89,24 +103,13 @@ public class Main {
     }
 
     public static void processValues(int numArray, int mode, int numRounds, String[][] set) {
+        score = 0;
+        times.clear();
+
         // Generates two random values for selecting the cards
         long startTime = System.nanoTime();
         // TODO add exception statement things in the main body
         String word = "";
-
-        long permittedTime = 0;
-        if (mode == 1 || mode == 2) {
-            permittedTime = Long.MAX_VALUE;
-        }
-        if (mode == 31) {
-            permittedTime = 1_000_000_000L * 25;
-        }
-        if (mode == 32) {
-            permittedTime = 1_000_000_000L * 20;
-        }
-        if (mode == 33) {
-            permittedTime = 1_000_000_000L * 15;
-        }
 
         if (mode == 1) {
             word = "letter";
@@ -116,84 +119,86 @@ public class Main {
             word = "letter";
         }
 
-        while (System.nanoTime() - startTime < permittedTime) {
-            int maxRows = 0;
+        int maxRows = 0;
 
-            if (numArray == 3) {
-                maxRows = 6;
-            } else if (numArray == 4) {
-                maxRows = 12;
-            }
-
-            for (int i = 0; i < numRounds; i++) {
-                int firstCardRow = random.nextInt(maxRows);
-                int secondCardRow = random.nextInt(maxRows);
-
-                // Prevents cards from being the same value
-                if (firstCardRow == secondCardRow) {
-                    firstCardRow += 1;
-                }
-                if (firstCardRow == maxRows) {
-                    firstCardRow -= 1;
-                }
-
-                // Picks card A at random
-                List<String> cardA = new ArrayList<>(Arrays.asList(set[firstCardRow]).subList(0, 4));
-                // Picks card B at random
-                List<String> cardB = new ArrayList<>(Arrays.asList(set[secondCardRow]).subList(0, 4));
-
-                // Shuffles Cards
-                Collections.shuffle(cardA);
-                Collections.shuffle(cardB);
-
-                // Prints Card A and Card B
-                for (String s : cardA) {
-                    print(s, 1);
-                }
-                print("", 0);
-
-                for (String s : cardB) {
-                    print(s, 1);
-                }
-                print("", 0);
-
-                // Gets the correct answer
-                cardB.retainAll(cardA);
-
-                print("What is the common " + word + "?", 0);
-                String guess = scanner.nextLine();
-
-                // Modifies the user input to be comparable to the arrayList
-                String guessMod = ("[" + guess + "]");
-
-                while (!guessMod.equalsIgnoreCase((String.valueOf(cardB)))) {
-                    print("incorrect! Please guess again", 0);
-                    guess = scanner.nextLine();
-                    guessMod = ("[" + guess + "]");
-                    if (guessMod.equalsIgnoreCase(String.valueOf(cardB))) {
-                        print("correct", 0);
-                        score += 1;
-                    }
-                    if (guess.equalsIgnoreCase("quit")) {
-                        print("The correct answer was: ", 0);
-                        for (String item : cardB) {
-                            print(item, 0);
-                            return;
-                        }
-                    }
-                }
-            }
-            return;
+        if (numArray == 3) {
+            maxRows = 6;
+        } else if (numArray == 4) {
+            maxRows = 12;
         }
-    }
+
+        for (int i = 0; i < numRounds; i++) {
+            int firstCardRow = random.nextInt(maxRows);
+            int secondCardRow = random.nextInt(maxRows);
+
+            // Prevents cards from being the same value
+            if (firstCardRow == secondCardRow) {
+                firstCardRow += 1;
+            }
+            if (firstCardRow == maxRows) {
+                firstCardRow -= 1;
+            }
+
+            // Picks card A at random
+            List<String> cardA = new ArrayList<>(Arrays.asList(set[firstCardRow]).subList(0, numArray));
+            // Picks card B at random
+            List<String> cardB = new ArrayList<>(Arrays.asList(set[secondCardRow]).subList(0, numArray));
+
+            // Shuffles Cards
+            Collections.shuffle(cardA);
+            Collections.shuffle(cardB);
+
+            // Prints Card A and Card B
+            for (String s : cardA) {
+                print(s, 1);
+            }
+            print("", 0);
+
+            for (String s : cardB) {
+                print(s, 1);
+            }
+            print("", 0);
+
+            // Gets the correct answer
+            cardB.retainAll(cardA);
+
+            print("What is the common " + word + "?", 0);
+            String guess = scanner.nextLine();
+
+            // Modifies the user input to be comparable to the arrayList
+            String guessMod = ("[" + guess + "]");
+
+            if (!guessMod.equalsIgnoreCase((String.valueOf(cardB)))) {
+                print("incorrect! Please guess again", 0);
+                guess = scanner.nextLine();
+                guessMod = ("[" + guess + "]");
+            }
+            else if (guessMod.equalsIgnoreCase(String.valueOf(cardB))) {
+                print("correct", 0);
+                }
+            else if (guess.equalsIgnoreCase("quit")) {
+                print("The correct answer was: ", 0);
+                for (String item : cardB) {
+                    print(item, 0);
+                    return;
+                }
+            }
+            int elapsedTime = (int) ((System.nanoTime() - startTime)/1_000_000_000);
+            times.add(elapsedTime);
+            }
+        }
+
 
 
     public static void customValues() {
+        print("Please input how many rounds you wish to play:", 0);
+        int numRounds = 0;
+        numRounds = scanner.nextInt();
 
         print("Please pick how many elements you want per card (A or B):", 0);
         print("---------------------------------------------------------", 0);
         print("      A)     3                             B)     4      ", 0);
-
+        scanner.nextLine();
         String temp = scanner.nextLine();
         int numElementsPerCard = 0;
 
@@ -206,10 +211,7 @@ public class Main {
             print("Please put in a valid input!", 0);
         }
 
-        int numRounds = 0;
         if (numElementsPerCard == 4) {
-            print("Please input how many rounds you wish to play:", 0);
-            numRounds = scanner.nextInt();
 
             print(BRIGHT_BACKGROUND_BLACK + BRIGHT_BLUE + "Please input thirteen names/items you wish to include", 0);
             String a = scanner.nextLine();
@@ -280,12 +282,12 @@ public class Main {
     public static int startMenu() {
         print(BRIGHT_BACKGROUND_BLACK + BRIGHT_WHITE + "Please select an option from one of the following:" + RESET, 0);
         print(BRIGHT_BACKGROUND_BLACK + BRIGHT_WHITE + "--------------------------------------------------" + RESET, 0);
-        print(BRIGHT_BACKGROUND_BLACK + BRIGHT_WHITE + "[1]  |"+ BRIGHT_BACKGROUND_BLACK + BRIGHT_MAGENTA +"                  Original                 " + BRIGHT_BACKGROUND_BLACK + BRIGHT_WHITE +"|" + RESET, 0);
-        print(BRIGHT_BACKGROUND_BLACK + BRIGHT_WHITE + "[2]  | " + BRIGHT_BACKGROUND_BLACK + BLUE + "               Input-based                " + BRIGHT_BACKGROUND_BLACK + BRIGHT_WHITE + "|" + RESET, 0);
-        print(BRIGHT_BACKGROUND_BLACK + BRIGHT_WHITE + "[3]  | " + BRIGHT_BACKGROUND_BLACK + CYAN + "            Timed Game Variant        " + BRIGHT_BACKGROUND_BLACK + BRIGHT_WHITE + "    |" + RESET, 0);
+        print(BRIGHT_BACKGROUND_BLACK + BRIGHT_WHITE + "[1]  |"+ BRIGHT_BACKGROUND_BLACK + BRIGHT_MAGENTA +"                 Original                  " + BRIGHT_BACKGROUND_BLACK + BRIGHT_WHITE +"|" + RESET, 0);
+        print(BRIGHT_BACKGROUND_BLACK + BRIGHT_WHITE + "[2]  | " + BRIGHT_BACKGROUND_BLACK + BLUE + "              Input-based                 " + BRIGHT_BACKGROUND_BLACK + BRIGHT_WHITE + "|" + RESET, 0);
+        print(BRIGHT_BACKGROUND_BLACK + BRIGHT_WHITE + "[3]  | " + BRIGHT_BACKGROUND_BLACK + CYAN + "           Timed Game Variant         " + BRIGHT_BACKGROUND_BLACK + BRIGHT_WHITE + "    |" + RESET, 0);
         //TODO figure out why this is broken
-        print(BRIGHT_BACKGROUND_BLACK + BRIGHT_WHITE + "[4]  | " + BRIGHT_BACKGROUND_BLACK + BRIGHT_YELLOW + "            Restore last score        " + BRIGHT_BACKGROUND_BLACK + BRIGHT_WHITE + "    |" + RESET, 0);
-        print(BRIGHT_BACKGROUND_BLACK + BRIGHT_WHITE + "[5]  |       " + BRIGHT_BACKGROUND_RED + BLUE + "Quit (you don't want too ooooo)" + BRIGHT_BACKGROUND_BLACK + BRIGHT_WHITE + "     |" + RESET, 0);
+        print(BRIGHT_BACKGROUND_BLACK + BRIGHT_WHITE + "[4]  | " + BRIGHT_BACKGROUND_BLACK + BRIGHT_YELLOW + "           Restore last score         " + BRIGHT_BACKGROUND_BLACK + BRIGHT_WHITE + "    |" + RESET, 0);
+        print(BRIGHT_BACKGROUND_BLACK + BRIGHT_WHITE + "[5]  |      " + BRIGHT_BACKGROUND_RED + BLUE + "Quit (you don't want too ooooo)" + BRIGHT_BACKGROUND_BLACK + BRIGHT_WHITE + "      |" + RESET, 0);
 
         int input;
 
@@ -294,7 +296,7 @@ public class Main {
             try {
                 input = scanner.nextInt();
                 scanner.nextLine();
-                while (input < 1 || input > 4) {
+                while (input < 1 || input > 5) {
                     print("That's an invalid input! Please put an integer between 1 and 4", 0);
                     input = scanner.nextInt();
                 }
@@ -308,7 +310,6 @@ public class Main {
     }
 
     public static void standardGame() {
-        score = 0;
 
         // Get value for number of rounds
         print(BRIGHT_BACKGROUND_BLACK + BRIGHT_MAGENTA + "Please enter the number of rounds you wish to play!", 0);
@@ -350,12 +351,18 @@ public class Main {
                     print("", 0);
                     print(RESET + "Done!", 0);
                     standardGame();
+                    print(times.toString(), 0);
+                    calculateScore(times.size());
+                    print(String.valueOf(score), 0);
                     break;
                 case 2:
                     printMsgWithProgressBar(GREEN + "Loading...", 25, 25);
                     print("", 0);
                     print(GREEN + "Done!", 0);
                     customValues();
+                    print(times.toString(), 0);
+                    calculateScore(times.size());
+                    print(String.valueOf(score), 0);
                     break;
                 case 3:
                     print("  Please enter the difficulty :)  ", 0);
@@ -375,7 +382,21 @@ public class Main {
                     timedGameVariant(difficulty);
                     break;
                 case 4:
-                    print("Your last score was: " + score, 0);
+                    if (score > 0) {
+                        print("Your last score was: " + score, 0);
+                        print("Please press 'q' to return to the main menu!", 0);
+                    }
+                    else {
+                        print("You haven't played yet!", 0);
+                        print("Please press 'q' to return to the main menu and get playing!", 0);
+                    }
+                    String returnMain = scanner.nextLine();
+                    if (returnMain.equalsIgnoreCase("q")) {
+                        printMsgWithProgressBar(RED + "Loading...", 25, 25);
+                        print("", 0);
+                        print(RED + "Done!", 0);
+                        break;
+                    }
                     break;
                 case 5:
                     int max = goodbyeMessages.length;
